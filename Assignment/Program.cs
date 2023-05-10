5ef067a91f219103a4ad634c10f9258f7a40f709
GwGibson <gibsonwgraham@gmail.com>
2023-05-10 17:10:52 -0400

    Fix formatting

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -40,9 +40,9 @@
     {
         static void Main()
         {
-            int myVariable = 10;
-                System.Console.WriteLine("Hello World");
-        System.Console.WriteLine("another line");
+            int myVariable =;
+            System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("another line");
             /*
             const int Min = 0;
             const int Max = 10;
